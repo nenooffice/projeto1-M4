977d0ac3e303ca5694760f419669b287cc21ed2f
Neno <eugenio.bufalorodrigues@gmail.com>
2022-06-27 19:17:01 -0300

    commit w/o

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/users.entities';
+import { uuid } from 'uuidv4';
+
+@Injectable()
+export class UsersService {
+  users: User[] = [];
+
+  getAll() {
+    return this.users;
+  }
+  create(createUserDto: CreateUserDto) {
+    const newUser: User = { id: uuid(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
