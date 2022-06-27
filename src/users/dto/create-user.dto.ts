0f019279c69b9d8c5730fcb83f6ff7a5f1dfea17
Neno <eugenio.bufalorodrigues@gmail.com>
2022-06-27 19:29:28 -0300

    Class Validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
 }
